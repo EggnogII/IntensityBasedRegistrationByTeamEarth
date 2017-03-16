218f4a3b56ed20f1b7a0d519c1b77e5cfc00a9db
Evan Wong <evan.wong00@gmail.com>
2017-03-15 18:22:02 -0700

    Check for a correct file input

--- src/project.cxx
+++ src/project.cxx
@@ -1,7 +1,14 @@
 #include "itkImageFileReader.h"
 #include "itkGDCMImageIO.h"
 
+#include <string>
 #include <iostream>
+
+bool is_file_exist(const char *fileName)
+{
+	std::ifstream infile(fileName);
+  return infile.good();
+}
 
 int main(int argc, char *argv[])
 {
@@ -14,6 +21,25 @@ int main(int argc, char *argv[])
     return EXIT_FAILURE;
     }
 
+	std::string inputCheck = argv[1];
+	if( inputCheck.find(".dcm") == std::string::npos)
+	{
+	std::cerr << argv[1] 
+						<< " needs to be dicom file format!"
+						<< std::endl;
+
+	return EXIT_FAILURE;
+	}
+
+	if(is_file_exist(argv[1]))
+	{
+		std::cerr << argv[1]
+							<< " does not exist!"
+							<< std::endl;
+
+		return EXIT_FAILURE;
+	}
+
     //specify dimensions 
     typedef signed short InputPixelType;
     const unsigned int InputDimension = 3;
@@ -23,6 +49,7 @@ int main(int argc, char *argv[])
     //read
     typedef itk::ImageFileReader<InputImageType> ReaderType;
 
+			
     ReaderType::Pointer reader = ReaderType::New();
     reader->SetFileName(argv[1]);
 	
